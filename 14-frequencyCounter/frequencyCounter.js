073711b6f52b5ef6fc083f7467c5ea2134d8bee1
Maxim Voyevoda <maximvoyevoda@gmail.com>
2023-06-12 00:14:41 -0400

    passed

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,5 +1,12 @@
 function frequencyCounter(word) {
-  // Insert code here;
+  let table = {}
+  for (const e of word){
+    if (!table[e])
+      table[e] = 1
+    else 
+      table[e] += 1
+  }
+  return table
 }
 
 // Do not edit this line;
