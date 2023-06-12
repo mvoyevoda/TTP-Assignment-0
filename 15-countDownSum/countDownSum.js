f2e683d1f7cf93536d2a78b8bf21e03d0171f28e
Maxim Voyevoda <maximvoyevoda@gmail.com>
2023-06-12 00:21:17 -0400

    passed

--- 15-countDownSum/countDownSum.js
+++ 15-countDownSum/countDownSum.js
@@ -1,6 +1,14 @@
 class MySolution {
   countDownSum(num) {
-    // Insert code here;
+    // let c = num
+    // for (let i=num-1; i >= 1; i--){
+    //   c += i
+    // }
+    // return c
+    if (num === 1)
+      return 1
+    else
+      return num + this.countDownSum(num-1)
   }
 }
 
